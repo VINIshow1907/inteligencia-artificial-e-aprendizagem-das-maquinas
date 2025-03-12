857477c780e8dea3736bd2d029817a0641b74b83
VINIshow1907 <viniciuslucio792@gmail.com>
2025-03-12 15:33:05 -0300

    :sparkles: Aprendendo matriz

---
+++ 1 SEMESTRE/Vetores e matrizes/matriz.py
@@ -0,0 +1,10 @@
+"primeiro a linha depois a coluna"
+vetor1 = ["bom dia","boa tarde", "boa noite"]
+vetor2 = ["Bruno", "Marceo", "Bernardo"]
+vetor3 = ["Como vai?", "Que horas são?", "Vamos programar?"]
+matriz =[vetor1,vetor2,vetor3]
+matriz.append(vetor1)
+matriz.append(vetor2)
+matriz.append(vetor3)
+
+print(matriz[0][1])
