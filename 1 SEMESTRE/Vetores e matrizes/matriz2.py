adecf3b15c8ef73425e6a6da7e0053d7afe96883
VINIshow1907 <viniciuslucio792@gmail.com>
2025-03-12 15:33:40 -0300

    :sparkles: exemplos em matriz

---
+++ 1 SEMESTRE/Vetores e matrizes/matriz2.py
@@ -0,0 +1,6 @@
+vetor1 = ["bom dia","boa tarde", "boa noite"]
+vetor2 = ["Bruno", "Marceo", "Bernardo"]
+vetor3 = ["Como vai?", "Que horas são?", "Vamos programar?"]
+matriz =[vetor1,vetor2,vetor3]
+matriz[0].append("Bom final de semana")
+print(matriz[0])
