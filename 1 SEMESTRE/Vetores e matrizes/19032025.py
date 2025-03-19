d84b6d1a148e6f3b1caff4afadf331913ab81779
VINIshow1907 <viniciuslucio792@gmail.com>
2025-03-19 15:08:57 -0300

    :sparkles: Matriz tridimencional, pegando sobrenome Hyuuga

---
+++ 1 SEMESTRE/Vetores e matrizes/19032025.py
@@ -0,0 +1,14 @@
+pessoas =[]
+nomes = ["Hinata","Naruto", "Sasuke"]
+sobrenomes =["Hyuuga", "Uzumaki", "Uchiha"]
+pessoas.append(nomes)
+pessoas.append(sobrenomes)
+lugares =[]
+estados = ["São Paulo", "Rio De Janeiro", "Sergipe", "Fortaleza"]
+capitais = ["São Paulo", "Rio De Janeiro", "Aracaju", "Ceará"]
+lugares.append(estados)
+lugares.append(capitais)
+conjunto =[]
+conjunto.append(pessoas)
+conjunto.append(lugares)
+print(conjunto[0][1][0])
